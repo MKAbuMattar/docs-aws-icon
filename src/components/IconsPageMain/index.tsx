182f4010015e622a731fb3c836b908d5404d0171
MKAbuMattar <mohammad.khaled@outlook.com>
2023-04-29 17:35:55 +0300

    fix: version 1.2.0

--- src/components/IconsPageMain/index.tsx
+++ src/components/IconsPageMain/index.tsx
@@ -29,21 +29,6 @@ const IconsPageMain = ({ icons, language = 'TypeScript' }: Props) => {
     setItems(initialItems(icons));
   }, []);
 
-  let globalPrefix = 'id';
-  let lastId = 0;
-  const nextgenerateUniqueKey = (localPrefix = '') => {
-    lastId++;
-    return `${localPrefix || globalPrefix}${lastId}`;
-  };
-
-  const generateUniqueNumber = () => {
-    return Math.floor(Math.random() * 100_000_000_000);
-  };
-
-  const generateUniqueKey = (pre) => {
-    return `${pre}_${nextgenerateUniqueKey()}_${new Date().getTime()}_${generateUniqueNumber()}_`.toLowerCase();
-  };
-
   return (
     <>
       <SearchSection id="search">
@@ -52,7 +37,7 @@ const IconsPageMain = ({ icons, language = 'TypeScript' }: Props) => {
 
       <Cards>
         {items.map((item: Daum) => (
-          <div key={generateUniqueKey(item.name)}>
+          <div key={item.name}>
             <Card item={item} language={language} />
           </div>
         ))}
