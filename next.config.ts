2498a975d8c6048fe1a413d2b0f9902c7d5a8d06
MKAbuMattar <mohammad.khaled@outlook.com>
2025-10-05 20:18:26 +0300

    feat: Add PWA support with next-pwa configuration

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import PWANextInit from 'next-pwa';
+import PWANextRuntimeCaching from 'next-pwa/cache';
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+} satisfies NextConfig;
+
+const PWANextConfig = PWANextInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: PWANextRuntimeCaching,
+  publicExcludes: [
+    '!robots.txt',
+    '!sitemap.xml',
+    '!sitemap.*.xml',
+    '!app-build-manifest.json',
+  ],
+  buildExcludes: ['app-build-manifest.json'],
+});
+
+export default PWANextConfig(nextConfig);
